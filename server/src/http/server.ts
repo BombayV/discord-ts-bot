ff4ff44930c9b4189d3be476702ae3148c75b5fe
Bombay <OMGmsebastian@gmail.com>
2023-12-13 15:00:36 -0500

    fix?

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,35 +1,30 @@
 import Fastify from 'fastify';
 import FastifyWebsocket from '@fastify/websocket'
 
-const fastify = Fastify({
-  logger: true
-})
-
+const fastify = Fastify();
 fastify.register(FastifyWebsocket);
-fastify.register(async function (fastify) {
-  fastify.route({
-    method: 'GET',
-    url: '/',
-    websocket: false,
-    handler: (request, reply) => {
-      reply.code(200).send({ message: 'Hello from Fastify!' });
-    },
-  })
 
-  fastify.route({
-    method: 'GET',
-    url: '/ws',
-    handler: (request, reply) => {
-      reply.code(200).send({ message: 'Use a WebSocket client to connect' });
-    },
-    wsHandler: (connection, request) => {
-      connection.socket.on('message', (message) => {
-        console.log('Received message: ', message);
-      });
-
-      connection.socket.send('Hello from WebSocket server');
-    },
-  });
+fastify.route({
+  method: 'GET',
+  url: '/',
+  websocket: false,
+  handler: (request, reply) => {
+    reply.code(200).send({ message: 'Hello from Fastify!' });
+  },
 })
 
+fastify.route({
+  method: 'GET',
+  url: '/ws',
+  handler: (request, reply) => {
+    reply.code(200).send({ message: 'Use a WebSocket client to connect' });
+  },
+  wsHandler: (connection, request) => {
+    connection.socket.on('message', (message) => {
+      console.log('Received message: ', message);
+    });
+
+    connection.socket.send('Hello from WebSocket server');
+  },
+});
 export default fastify;
